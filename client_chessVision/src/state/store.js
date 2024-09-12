56d979909477bc2e7cc964d571f981b23acef49a
Rahul_Pharthyal_MNNLR <mern.mnnlr.rahulpharthyal@gmail.com>
2024-09-12 12:00:08 +0530

    Delete client_chessVision directory

--- client_chessVision/src/state/store.js
+++
@@ -1,9 +0,0 @@
-// src/store.js
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import loginReducer from './slices/loginSlice';
-
-const rootReducer = combineReducers({
-  login: loginReducer,
-});
-
-export const store = configureStore({reducer: rootReducer})
