436fe346cc7cd9ca433f30a54dab6b9f98ec8cac
muskanY2 <mern.muskanydv@gmail.com>
2025-02-13 12:29:28 +0530

    updated playbuttons and move features

--- src/Pages/Chessq.js
+++ src/Pages/Chessq.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ChessGame from './ChessGame';
-import { FaStepForward , FaPlay, FaStepBackward} from "react-icons/fa";
+import { FetchgamesByDate } from "../utils/FetchgamesByDate";
+import { AnalyseGame } from "../utils/AnalyseGames";
 
 const Chessq = () => {
   const [moves, setMoves] = useState([]);
@@ -47,16 +48,17 @@ const Chessq = () => {
           </div>
 
           {/* Playback Buttons Fixed at Bottom */}
-          <div className="bg-gray-800 p-3 rounded-lg flex justify-between w-full ">
-            <button className="bg-gray-700 p-2 rounded hover:bg-gray-600">
-            <FaStepBackward  size={20} />
-            </button>
-            <button className="bg-gray-700 p-2 rounded hover:bg-gray-600">
-              <FaPlay size={20} />
-            </button>
-            <button className="bg-gray-700 p-2 rounded hover:bg-gray-600">
-            <FaStepForward   size={20} />
-            </button>
+          {/* Playback Controls */}
+          <div className="bg-gray-800 p-3 rounded-lg flex justify-between">
+              <button className="bg-gray-700 p-1 rounded hover:bg-gray-600"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M7 7h10v3l4-4l-4-4v3H5v6h2zm10 10H7v-3l-4 4l4 4v-3h12v-6h-2z" /></svg></button>
+              <button className="bg-gray-700 p-1 rounded hover:bg-gray-600"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M6 6h2v12H6zm3.5 6l8.5 6V6z" /></svg></button>
+              <button className="bg-gray-700 p-1 rounded hover:bg-gray-600"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="M20 11H7.83l5.59-5.59L12 4l-8 8l8 8l1.41-1.41L7.83 13H20z" /></svg></button>
+
+              <button className="bg-gray-700 p-1 rounded hover:bg-gray-600"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m10 17l5-5l-5-5z" /></svg></button>
+              <button className="bg-gray-700 p-1 rounded hover:bg-gray-600"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z" /></svg></button>
+
+              <button className="bg-gray-700 p-1 rounded hover:bg-gray-600"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m6 18l8.5-6L6 6zM16 6v12h2V6z" /></svg></button>
+           
           </div>
         </div>
       </div>
