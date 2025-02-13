6694de618adf33f6757d473644604c00f942c099
muskanY2 <mern.muskanydv@gmail.com>
2025-02-13 12:30:15 +0530

    Update Chessq.js

--- src/Pages/Chessq.js
+++ src/Pages/Chessq.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ChessGame from './ChessGame';
-import { FetchgamesByDate } from "../utils/FetchgamesByDate";
-import { AnalyseGame } from "../utils/AnalyseGames";
+
 
 const Chessq = () => {
   const [moves, setMoves] = useState([]);
